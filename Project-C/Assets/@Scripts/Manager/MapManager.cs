887f7ef16a2b62c7001cd3b535095bec4d31c4a3
멍멍민 <pr620718@cu.ac.kr>
2025-02-06 12:20:39 +0900

    ✨ Refactor: #22 시네머신 카메라로 추적 및 월드 경계 제한
    - 기존의 플레이어 위치와 맵 경계를 계산하여 카메라를 이동하는 방식에서 시네머신 카메라로 전환
      - CameraBounds를 기반으로 카메라가 월드 경계를 벗어나지 않는다.
    - 공식 문서: https://docs.unity3d.com/Packages/com.unity.cinemachine@3.1/manual/CinemachineBasicMultiChannelPerlin.html

--- Project-C/Assets/@Scripts/Manager/MapManager.cs
+++ Project-C/Assets/@Scripts/Manager/MapManager.cs
@@ -8,10 +8,11 @@ public class MapManager
     public GameObject Map { get; private set; }
     public string MapName { get; private set; }
     public Grid CellGrid { get; private set; }
+    public PolygonCollider2D CameraBounds { get; private set; } // 카메라의 이동 범위 제한
 
     // 맵 경계
-    public Vector2 MinBound { get; private set; }
-    public Vector2 MaxBound { get; private set; }
+    //public Vector2 MinBound { get; private set; }
+    //public Vector2 MaxBound { get; private set; }
 
     public void LoadMap(string mapName)
     {
@@ -25,8 +26,21 @@ public class MapManager
         MapName = mapName;
         CellGrid = map.GetComponent<Grid>();
 
+        foreach (PolygonCollider2D bounds in map.GetComponentsInChildren<PolygonCollider2D>())
+        {
+            if (bounds.name.Contains("CameraBounds"))
+            {
+                CameraBounds = bounds;
+                break;
+            }
+        }
+
+        // 카메라 위치를 월드 경계로 제한
+        CameraController camera = Camera.main.GetComponent<CameraController>();
+        camera.Confiner.m_BoundingShape2D = CameraBounds;
+
         // 모든 타일맵의 경계 계산
-        CalculateMapBounds();
+        //CalculateMapBounds();
     }
 
     public void DestroyMap()
@@ -38,31 +52,31 @@ public class MapManager
     /// <summary>
     /// 타일맵의 경계를 계산한다.
     /// </summary>
-    void CalculateMapBounds()
-    {
-        // 배경 타일맵을 찾는다
-        Tilemap backgroundTilemap = null;
-        foreach (Tilemap tilemap in Map.GetComponentsInChildren<Tilemap>())
-        {
-            if (tilemap.name.Contains("Background"))
-            {
-                backgroundTilemap = tilemap;
-                break;
-            }
-        }
+    //void CalculateMapBounds()
+    //{
+    //    // 배경 타일맵을 찾는다
+    //    Tilemap backgroundTilemap = null;
+    //    foreach (Tilemap tilemap in Map.GetComponentsInChildren<Tilemap>())
+    //    {
+    //        if (tilemap.name.Contains("Background"))
+    //        {
+    //            backgroundTilemap = tilemap;
+    //            break;
+    //        }
+    //    }
 
-        if (backgroundTilemap == null)
-            return;
+    //    if (backgroundTilemap == null)
+    //        return;
 
-        // 배경 타일맵의 셀 경계를 월드 좌표로 변환
-        BoundsInt cellBounds = backgroundTilemap.cellBounds;
-        Vector3 worldMin = backgroundTilemap.CellToWorld(cellBounds.min);   // 제일 왼쪽 아래
-        Vector3 worldMax = backgroundTilemap.CellToWorld(cellBounds.max);  // 제일 오른쪽 위
+    //    // 배경 타일맵의 셀 경계를 월드 좌표로 변환
+    //    BoundsInt cellBounds = backgroundTilemap.cellBounds;
+    //    Vector3 worldMin = backgroundTilemap.CellToWorld(cellBounds.min);   // 제일 왼쪽 아래
+    //    Vector3 worldMax = backgroundTilemap.CellToWorld(cellBounds.max);  // 제일 오른쪽 위
 
-        // 맵 경계 설정
-        MinBound = new Vector2(worldMin.x, worldMin.y);
-        MaxBound = new Vector2(worldMax.x, worldMax.y);
+    //    // 맵 경계 설정
+    //    MinBound = new Vector2(worldMin.x, worldMin.y);
+    //    MaxBound = new Vector2(worldMax.x, worldMax.y);
         
-        Debug.Log($"Map Bounds - Min: {MinBound}, Max: {MaxBound}");
-    }
+    //    Debug.Log($"Map Bounds - Min: {MinBound}, Max: {MaxBound}");
+    //}
 }
