df0a96a6a68da1c24c2c83887ee3bf7c9c467d0a
juhyun <gjhy1123@gmail.com>
2025-02-10 23:52:26 +0900

    #19 몬스터 네비게이션 생성
    몬스터 네비게이션 생성

--- Project-C/Assets/@Scripts/Character/Monster/Navigator.cs
+++ Project-C/Assets/@Scripts/Character/Monster/Navigator.cs
@@ -1,17 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.HableCurve;
 
 
 [SerializeField]
 class Node : IComparable<Node>
 {
-    public int x, y, G, H;
-    public int F { get { return G + H; } }
+    public float x, y, G, H;
+    public bool _isBlocking;
+    public float F { get { return G + H; } }
+
+    public Node(float x, float y, bool isBlocking) 
+    {
+        this.x = x;
+        this.y = y;
+        this._isBlocking = isBlocking;
+    }
 
     public int CompareTo(Node node)
     {
@@ -100,21 +109,105 @@ public class Navigator : MonoBehaviour
     }
     
     [SerializeField] private EPathType _pathType;
-    [SerializeField] private int _nodeSize;
-    [SerializeField] private int _maxDistance;
+    [SerializeField] private float _nodeSize = 0;
+    [SerializeField] private float _collisionBuffer = 0;
+    [SerializeField] private int _maxDistance = 0;
     [SerializeField] private bool _diagonalCheck;
+    [SerializeField] private string[] _blockLayers;
+    
 
+    [SerializeField] GameObject target;
 
-    private List<Node> confirmedPath;
+    private List<Node> _confirmedPath;
+    private Node[,] _map;
+    private string _blockStr;
 
     public EPathType PathType { get { return _pathType; } }
 
+    private void makingBlockingLayerString()
+    {
+        _blockStr = string.Join(",", _blockLayers);
+        Debug.Log(_blockStr);
+    }
 
-    public bool FindPath()
+    void Start()
+    {
+        FindPath(this.transform.position, target.transform.position);
+    }
+
+
+    public bool FindPath(Vector2 mover, Vector2 target)
     {
         
+        if (_nodeSize == 0 || _maxDistance == 0 || _blockLayers == null) return false;
+        if(Vector2.Distance(mover, target) > _nodeSize * _maxDistance) return false;
         
+        if(_blockStr == null) makingBlockingLayerString();
+        
+        int mapMaxSizeX = _maxDistance * 2 + 1;
+        int mapMaxSizeY = _maxDistance * 2 + 1;
+
+        _map = new Node[mapMaxSizeY, mapMaxSizeX];
+
+
+        for(int y = 0; y < mapMaxSizeY; y++)
+        {
+            for(int x = 0; x < mapMaxSizeX; x++)
+            {
+                float posX = ((_maxDistance - x) * _nodeSize) + mover.x;
+                float posY = ((_maxDistance - y) * _nodeSize) + mover.y;
+                bool isBlocking = Physics2D.OverlapCircle(new Vector2(posX, posY), _nodeSize / 2 - (_collisionBuffer), LayerMask.GetMask(_blockLayers));
+
+                _map[y, x] = new Node(posX, posY, isBlocking);
+                positions.Add(new Vector2(posX, posY)); // test
+            }
+        }
+
+        
+
+
+
+
         return false;
         
     }
+
+
+
+
+    /*
+     * 테스트 용
+     */
+    public List<Vector2> positions = new List<Vector2>();  // 여러 개의 원의 중심 좌표
+    
+
+    public void Update()
+    {
+        foreach (Node node in _map)
+        {
+            Color col = Color.green;
+            if (node._isBlocking) col = Color.red;
+
+            DrawCircle(new Vector2(node.x, node.y), _nodeSize / 2 - (_collisionBuffer), col);
+        }
+    }
+
+    private void DrawCircle(Vector2 center, float radius, Color color, int segments = 36)
+    {
+        
+        float angleStep = 360f / segments;
+        Vector3 prevPoint = center + new Vector2(radius, 0);
+
+        for (int i = 1; i <= segments; i++)
+        {
+            float angle = angleStep * i * Mathf.Deg2Rad;
+            Vector3 newPoint = center + new Vector2(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+
+            Debug.DrawLine(prevPoint, newPoint, color, 0.02f);
+            prevPoint = newPoint;
+        }
+    }
+
+
+
 }
