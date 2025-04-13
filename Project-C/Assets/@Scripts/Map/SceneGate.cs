5683b9e0f064ef6b4c2d74b6bd068a1d6d34eaa0
멍멍민 <pr620718@cu.ac.kr>
2025-04-14 00:02:58 +0900

    🚀 Feat: #26 SceneGate 구현
    - SceneGate 오브젝트 생성
    - 플레이어가 SceneGate와 접촉하면 지정된 다음 씬으로 자동 전환되도록 구현

---
+++ Project-C/Assets/@Scripts/Map/SceneGate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class SceneGate : InitBase
+{
+    public EScene TransitionSceneType = EScene.None;    // 인스펙터 창에서 직접 설정
+    
+    BoxCollider2D _collider;
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        _collider = GetComponent<BoxCollider2D>();
+        _collider.isTrigger = true;
+
+        // 트리거 필터링
+        LayerMask includeLayers = 0;
+        includeLayers.AddLayer(ELayer.Player);
+        _collider.includeLayers = includeLayers;
+
+        return true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{TransitionSceneType.ToString()} 전환");
+        Managers.Game.Save();
+        // TODO: 씬 전환하는 연출 필요, (ex: 플레이어가 걸어가는 모습, 까맣게 암전되는 모습 등)
+        Managers.Scene.LoadScene(TransitionSceneType);
+    }
+}
